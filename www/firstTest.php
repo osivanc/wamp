3a5da035d0a0a61cc4f3cf450bd75fec255d04e1
Oscar I A Chavarria <osivanc@utexas.edu>
2014-12-11 23:00:43 -0500

    successfully accessed data in tables in test_db

--- www/firstTest.php
+++ www/firstTest.php
@@ -1,28 +1,44 @@
 <html>
-  <head>
-    <title>Solo For Loop!</title>
-  </head>
-  <body>
-    <p>
-      <?php
+<head>
+	<title>Solo For Loop!</title>
+</head>
+<body>
+	<p>
+		<?php
+		//Vars
+		$helloE = "Hello world echoed";
+		$hellop = "Hello world printed";
+		$user_name = "root";
+		$password = "";
+		$database = "test_db";
+		$server = "127.0.0.1";
+		//print is slower than echo apparently
+		print $hellop . "<BR>";
+		echo $helloE . "<BR>";
 
-      	$helloE = "Hello world echoed";
-      	$hellop = "Hello world printed";
-			$user_name = "root";
-			$password = "";
-			$database = "addressbook";
-			$server = "127.0.0.1";
+		//connect to set server and database
+		$link = mysqli_connect($server, $user_name, $password, $database);
+		if ($link) { 
+			print "Connection to the Server opened <BR>";
 
-			print $hellop;
+			$SQL = "SELECT * FROM emp";
+			$result = mysqli_query($link, $SQL);
+			//Execute execute and fetch
+			while ($row = mysqli_fetch_assoc($result)) {
+				print $row['id'] . " " . $row['f_name'] . "<BR>";
+			}
+		} 
+		//if connection failed
+		else {
+			die('Could not connect to MySQL: ' . mysql_error()); 
+		}
 
-			mysqli_connect($server, $user_name, $password);
-			
-
-
-			print "Connection to the Server opened";
-
+		//close connection, good practice
+		if ($link){
+			mysqli_close($link);
+		}
 		?>
 		<h1>How now brown cow</h1>
-    </p>
-  </body>
+	</p>
+</body>
 </html>
