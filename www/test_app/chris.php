bd06fec1b7fe5d0e4c1ea46ac1da9825ee620efc
Oscar I A Chavarria <osivanc@utexas.edu>
2015-03-16 19:42:32 -0500

    test_app and test_app2 working

---
+++ www/test_app/chris.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>This is Chris</title>
+	</head>
+	<body>
+		<h1>Chris</h1>
+	</body>
+</html>
---
+++ www/test_app/oscar.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>This is Oscar</title>
+	</head>
+	<body>
+		<h1>Oscar</h1>
+	</body>
+</html>
